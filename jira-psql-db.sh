b66b1a8530017dc1f8631da653d047861cd193db
Kyle Manel <kyletmanel@gmail.com>
2022-03-25 06:35:44 -0400

    Update jira-psql-db.sh

--- jira-psql-db.sh
+++ jira-psql-db.sh
@@ -1,5 +1,5 @@
 PG_PASSWORD="t818_880u10_63_R3910c30
 APP_PW="bogus_password"
 
-docker exec -e PGPASSWORD="${PG_PASSWORD}" -u postgres postgres psql -U postgres -c "CREATE USER jira WITH PASSWORD 't81515n756F3';"
+docker exec -e PGPASSWORD="${PG_PASSWORD}" -u postgres postgres psql -U postgres -c "CREATE USER jira WITH PASSWORD 'bogus_password';"
 docker exec -e PGPASSWORD="${PG_PASSWORD}" -u postgres postgres psql -U postgres -c "CREATE DATABASE jiradb WITH ENCODING 'UNICODE' LC_COLLATE 'C' LC_CTYPE 'C' TEMPLATE template0;"
